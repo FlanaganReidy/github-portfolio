1e4ac65bdf953bc7867d4954770996eef835b8e6
Shannon Reidy <shannonf.reidy@gmail.com>
2021-01-10 16:02:11 -0600

    added basic styling

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import './Profile.css';
+import Link from '../components/Link/Link.js';
 
 class Profile extends Component {
     constructor(){
@@ -26,16 +28,18 @@ async componentDidMount() {
         }
         return(
             <div>
+                <div className='Profile-container'>
+                    <img className='Profile-avatar' src={data.avatar_url} alt='avatar' />
                 <ul>
-                    <li><img src={data.avatar_url} alt="profile"></img></li>
-                    <li>html_url: {data.html_url}</li>
-                    <li>repos_url: {data.repos_url}</li>
-                    <li>name: {data.name}</li>
-                    <li>company: {data.company}</li>
-                    <li>location: {data.location}</li>
-                    <li> email: {data.email}</li>
-                    <li>bio: {data.bio}</li>
+                    <li><strong>html_url:</strong> <Link url={data.html_url} title="Github URL" /></li>
+                    <li><strong>repos_url:</strong> <Link url={data.repos_url} title="Repos URL" /></li>
+                    <li><strong>name:</strong> {data.name}</li>
+                    <li><strong>company:</strong> {data.company}</li>
+                    <li><strong>location:</strong> {data.location}</li>
+                    <li> <strong>email:</strong> {data.email}</li>
+                    <li><strong>bio:</strong>{data.bio}</li>
                 </ul>
+            </div>
             </div>
         )
     }
