70348ccb09ce71fad6132b64eef7546787fdef09
Shannon Reidy <shannonf.reidy@gmail.com>
2021-01-11 22:42:27 -0600

    created link behavior for mapping data

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './Profile.css';
 import Link from '../components/Link/Link.js';
+import List from '../components/List/List.js';
 
 class Profile extends Component {
     constructor(){
@@ -26,19 +27,16 @@ async componentDidMount() {
         if (loading){
             return <div>Loading...</div>
         }
+        const items =[{label : 'html_url', value: <Link url={data.html_url} title="Github URL" />},
+        {label:'repos_url', value: data.repos_url},{label:'Name', value:data.name},{label:'Location', value: data.location},
+        {label:'Email', value:data.email}, {label:'Bio', value:data.bio}
+        ];
+
         return(
             <div>
                 <div className='Profile-container'>
                     <img className='Profile-avatar' src={data.avatar_url} alt='avatar' />
-                <ul>
-                    <li><strong>html_url:</strong> <Link url={data.html_url} title="Github URL" /></li>
-                    <li><strong>repos_url:</strong> <Link url={data.repos_url} title="Repos URL" /></li>
-                    <li><strong>name:</strong> {data.name}</li>
-                    <li><strong>company:</strong> {data.company}</li>
-                    <li><strong>location:</strong> {data.location}</li>
-                    <li> <strong>email:</strong> {data.email}</li>
-                    <li><strong>bio:</strong>{data.bio}</li>
-                </ul>
+                    <List items={items} />
             </div>
             </div>
         )
